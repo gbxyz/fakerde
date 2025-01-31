a539271471ab30866c78c0bd441c69754b7b849f
Gavin Brown <gavin.brown@fastmail.uk>
2025-01-31 13:51:00 +0000

    follow redirects

--- fakerde.php
+++ fakerde.php
@@ -471,6 +471,7 @@ final class generator {
         } else {
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/115.0');
 
